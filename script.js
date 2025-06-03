bd4c3cac8e7f7fe598ed5e9d4519703d2f545f7c
BautistaDelbino <b4utistadelbino@gmail.com>
2025-06-02 22:35:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,17 +6,18 @@ const pages = [
     image: 'img/iglesia1.jpeg',
     imageWidth: '45%',
     textTop: 'el 8 de julio de 1888, la fecha de fundación de la Congregación Evangélica de Humboldt, y el día 16 de julio comienza funcionar la escuela Estos acontecimientos hablan de la decisión de las familias protestantes para mejorar la instrucción de sus hijos, con una clara visión de que la religión y la educación son las mejores herramientas para entenderse y trabajar en ésta, para ellos, nueva patria por adopción. El presidente de la primera Comisión Directiva fue Santiago Deppeler. El pastor Forster venía a celebrar los cultos, pero en casa de familias, porque no existía un lugar físico determinado para ello.',
-    textMiddle: 'Se levanta la construcción en tiempo récord, ya que en el acta del 14-3 de 1889 se hace mención del organista proposición hecha a éste por el término de 3 meses tocar el órgano en el Templo. Este Templo humilde, levantado con dedicación, entusiasmo y alegría, fue el guardián de la Palabra del Señor y el lugar al que concurrían las familias para dar tes-fimonio de su fe y renovar sus esperanzas, ya que el trabajo era duro en estas tierras en que decidieron afincarse y forjar su por-venir. Los encuentros con la Santa Cena y los sermones de los Pastores, marcaban la senda que no se debía abandonar. El viejo templo fue testigo de cultos, bautismos, bendiciones nupciales, reuniones de comisiones y asambleas, confirma-ciones, ensayos de coro, fiesta de Navidad, y toda otra actividad que estuviera ligada al diario quehacer de la Comunidad.',
-    textBottom: 'En mayo de 1937, se nombra una comisión constructora para tal fin, se piden pa-receres, se discuten ofertas y se acepta que un arquitecto alemán se haga cargo de la nueva obra. Se trata del joven Peter Bartning de 24 años, que originario de Berlín viaja a bordo de un barco que parte de Hamburgo, Alemania el 19 de noviembre de 1937',
+    textMiddle: 'Se levanta la construcción en tiempo récord, ya que en el acta del 14-3 de 1889 se hace mención del organista proposición hecha a éste por el término de 3 meses tocar el órgano en el Templo.',
+    
+    textBottom: ' ',
   },
   {
     bgColor: '#355C7D',
     imageSide: 'right',
-    image: 'WhatsApp Image 2025-05-13 at 4.03.22 PM.jpeg',
-    imageWidth: '50%',
-    textTop: 'Otra página comienza con un párrafo introductorio.',
-    textMiddle: 'Texto central que acompaña visualmente a la imagen a su lado.',
-    textBottom: 'Y una conclusión o nota final debajo de todo.'
+    image: 'img/organo.jpeg',
+    imageWidth: '45%',
+    textTop: 'Este Templo humilde, levantado con dedicación, entusiasmo y alegría, fue el guardián de la Palabra del Señor y el lugar al que concurrían las familias para dar tes-fimonio de su fe y renovar sus esperanzas, ya que el trabajo era duro en estas tierras en que decidieron afincarse y forjar su por-venir. Los encuentros con la Santa Cena y los sermones de los Pastores, marcaban la senda que no se debía abandonar.',
+    textMiddle: '  El viejo templo fue testigo de cultos, bautismos, bendiciones nupciales, reuniones de comisiones y asambleas, confirma-ciones, ensayos de coro, fiesta de Navidad, y toda otra actividad que estuviera ligada al diario quehacer de la Comunidad. ',
+    textBottom: ' En mayo de 1937, se nombra una comisión constructora para tal fin, se piden pa-receres, se discuten ofertas y se acepta que un arquitecto alemán se haga cargo de la nueva obra. Se trata del joven Peter Bartning de 24 años, que originario de Berlín viaja a bordo de un barco que parte de Hamburgo, Alemania el 19 de noviembre de 1937',
   },
   {
     bgColor: '#355C7D',
