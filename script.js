39326ecaf67e8fcde018068ffc17ce1e11d9aa0c
BautistaDelbino <b4utistadelbino@gmail.com>
2025-08-08 08:54:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -31,8 +31,9 @@ const pages = [
   image2Side: 'right',
   image2: '',
   image2Width: '40%',
-    textTop: 'Se levanta la construcción en tiempo récord, ya que en el acta del 14-3 de 1889 se hace mención del organista proposición hecha a éste por el término de 3 meses tocar el órgano en el Templo. Este Templo humilde, levantado con dedicación, entusiasmo y alegría, fue el guardián de la Palabra del Señor y el lugar al que concurrían las familias para dar tes-fimonio de su fe y renovar sus esperanzas, ya que el trabajo era duro en estas tierras',
-    textMiddle: 'en que decidieron afincarse y forjar su por-venir. Los encuentros con la Santa Cena y los sermones de los Pastores, marcaban la senda que no se debía abandonar. El viejo templo fue testigo de cultos, bautismos, bendiciones nupciales, reuniones de comisiones y asambleas, confirma-ciones, ensayos de coro, fiesta de Navidad, y toda otra actividad que estuviera ligada al diario quehacer de la Comunidad.',
+  title: 'El Templo',
+    textTop: 'Se levanta la construcción en tiempo récord, ya que en el acta del 14-3 de 1889 se hace mención del organista proposición hecha a éste por el término de 3 meses tocar el órgano en el Templo. Este Templo humilde, levantado con dedicación, entusiasmo y alegría, fue el guardián de la Palabra del Señor y el lugar al que concurrían las familias para dar testimonio de su fe y renovar sus esperanzas',
+    textMiddle: 'Los encuentros con la Santa Cena y los sermones de los Pastores, marcaban la senda que no se debía abandonar. El viejo templo fue testigo de cultos, bautismos, reuniones de comisiones y asambleas, confirmaciones, ensayos de coro, navidades, y toda otra actividad.',
     textBottom: ''
   },
   // Pagina 3
@@ -44,9 +45,10 @@ const pages = [
     image2Side: 'right',
     image2: '',
     image2Width: '40%',
-    textTop: 'En mayo de 1937, se nombra una comisión constructora para tal fin, se piden pa-receres, se discuten ofertas y se acepta que un arquitecto alemán se haga cargo de la nueva obra. Se trata del joven Peter Bartning de 24 años, que originario de Berlín viaja a bordo de un barco que parte de Hamburgo, Alemania el 19 de noviembre de 1937',
-    textMiddle: 'El Arquitecto Peter, era alojado en la zona rural, a esta obra tan esperada se decide colocar la piedra fundamental, y este acto trascendental ocurrió el 15 de abril, viernes santo de ese 1938, con mucho frío y lluvia. Con fecha 4 de agosto de 1938, el joven Arquitecto Peter Bartning entrega una carta recibo, donde da por concluida la obra.',
-    textBottom: 'Se decide en reunión de Comisión Di-rectiva, que la inauguración del nuevo templo se realice en el mes de octubre y se festejen también los 50 años de la Congre-gacion.'
+    title: 'Peter Bartning',
+    textTop: 'En mayo de 1937, se nombra una comisión constructora y se acepta que un arquitecto alemán se haga cargo de la nueva obra. Se trata del joven Peter Bartning de 24 años, que originario de Berlín viaja a bordo de un barco, el 19 de noviembre de 1937',
+    textMiddle: 'A esta obra tan esperada se decide colocar la piedra fundamental, y este acto trascendental ocurrió el 15 de abril, viernes santo de ese 1938, con mucho frío y lluvia. Con fecha 4 de agosto de 1938, el joven Arquitecto Peter Bartning entrega una carta recibo, donde da por concluida la obra.',
+    textBottom: 'Se decide en reunión, que la inauguración se realice en el mes de octubre y se festejen también los 50 años de la Congregacion.'
   },
   // Pagina 4
 {
@@ -57,9 +59,10 @@ const pages = [
     image2Side: 'right',
     image2: '',
     image2Width: '40%',
+    title: 'Campanario',
     textTop: 'culto de inauguración del día 23 de octubre de 1938, La nueva construcción tiene campana-110 pero no tiene campanas. Es entonces que comienza un largo trámite que duraria nada menos que 10 años. ',
-    textMiddle: 'Se reciben además aportes de la Comunidad Evangélica de Esperanza y la Comisión de Fo-mento. Con la venta de los materiales de la demolición del Antiguo Templo, se cubre la deuda total, demostrando así que "nada se pierde todo se transforma" La energía de paredes y techos, las ri-sas, cantos y llantos de que estaban im-pregnados, no se fueron con los carros que llevaban los productos de la demolición.',
-    textBottom: 'Quedaron acá para siempre en estos testigos musicales que dan sus notas al aire, diciendo "somos aquellos que fuimos y seremos en la historia y el recuerdo". El 12 de noviembre de 1950 fueron consagradas las campanas en un culto especial. en mayo de 1959, se radica el Pastor Otto Vohringer con su tamilia'
+    textMiddle: 'Se reciben además aportes de la Comunidad Evangélica de Esperanza y la Comisión de Fomento. Con la venta de los materiales de la demolición del Antiguo Templo, se cubre la deuda total, demostrando así que "nada se pierde todo se transforma" La energía de paredes y techos, las risas, cantos y llantos de que estaban impregnados, no se fueron con los carros que llevaban los productos de la demolición.',
+    textBottom: 'Quedaron acá para siempre en estos testigos musicales que dan sus notas al aire, diciendo "somos aquellos que fuimos y seremos en la historia y el recuerdo". El 12 de noviembre de 1950 fueron consagradas las campanas. En mayo de 1959, se radica el Pastor Otto Vohringer con su tamilia'
   },
   // Pagina 5
 {
@@ -70,9 +73,10 @@ const pages = [
     image2Side: 'right',
     image2: '',
     image2Width: '40%',
-    textTop: '22 de marzo de 1928, se formó la primera comisión directiva de la Sociedad de Damas Evangélicas, el rol de estas mujeres era muy impor-tante, se preocupaban por las distintas áreas de los ministerios de la iglesia, como por ejemplo la catequesis, incluyendo clases de confirmación, escuela dominical, obligaciones de los cantantes del Coro de la Iglesia, la escuela de alemán.',
-    textMiddle: 'Ellas eran quienes recaudaban fondos para cubrir estos gastos, además fondos para el sostenimiento de la iglesia y mantenimiento de las instalaciones. Damas, junto con la familia del pastor Johan Vöhringer, celebraron Navidad y en ese culto fue la primera Fiesta del Arboli-to, que hasta la actualidad se sigue con la tradición de adornar un árbol natural para dicha celebración.',
-    textBottom: 'Uno de sus grandes anhelos fue construir un espacio para reuniones y demás actividades relacionadas con la iglesia. El 8 de julio de 1988, recordando el primer Centenario de la Comunidad Evan-gélica, se inauguró el salón parroquial,'
+    title: 'Damas Evangélicas',
+    textTop: '22 de marzo de 1928, se formó la primera comisión directiva de la Sociedad de Damas Evangélicas, su rol era fundamental, se preocupaban por las distintas áreas de los ministerios de la iglesia, como por ejemplo la catequesis, clases de confirmación, escuela dominical, Coro de la Iglesia, escuela de alemán.',
+    textMiddle: 'Ellas eran quienes recaudaban fondos para cubrir gastos, además para el sostenimiento de la iglesia y mantenimiento de las instalaciones. Damas, junto con la familia del pastor Johan Vöhringer, celebraron Navidad y en ese culto fue la primera Fiesta del Arbolito, que hasta la actualidad se sigue con la tradición de adornar un árbol natural.',
+    textBottom: 'Uno de sus grandes anhelos fue construir un espacio para reuniones y demás actividades relacionadas con la iglesia. El 8 de julio de 1988, recordando el primer Centenario de la Comunidad Evangélica, se inauguró el salón parroquial,'
   },
   // Pagina 6
 {
@@ -83,9 +87,24 @@ const pages = [
     image2Side: 'right',
     image2: 'img/escuela2.jpeg',
     image2Width: '80%',
-    textTop: 'Escuelita Biblica: La educación cristiana es uno de los pilares importantes para el crecimiento de la fe. Y es el espacio dedicado a nuestros pequeños y adolescentes. Con ellos compartimos encuentros semanales, donde después de cantos, oraciones, ofrendas, se los divide por edades para compartir y',
-    textMiddle: 'aprender de las historias bíblicas como así también distintos temas que tienen que ver con la vida cotidiana. Ellos participan de encuentros, campamentos y cultos espe-ciales. Estamos llamados a compartir la fe de generación en generación y así cumplir con el mandato de nuestro señor Jesús.',
+    title: 'Escuelita Bíblica',
+    textTop: 'La educación cristiana es uno de los pilares importantes para el crecimiento de la fe. Un espacio dedicado a nuestros pequeños y adolescentes. Con ellos compartimos encuentros semanales, donde después de cantos, oraciones, ofrendas, se los divide por edades para compartir y',
+    textMiddle: 'aprender de las historias bíblicas como así también distintos temas que tienen que ver con la vida cotidiana. Ellos participan de encuentros, campamentos y cultos especiales. Estamos llamados a compartir la fe de generación en generación y así cumplir con el mandato de nuestro señor Jesús.',
     textBottom: ''
+  },
+  // Pagina 7
+{
+    bgColor: '#355C7D',
+    image1Side: 'left',
+    image1: 'diaconia.jpeg',
+    image1Width: '100%',
+    image2Side: 'right',
+    image2: '',
+    image2Width: '',
+    title: 'Grupo de Diaconía',
+    textTop: 'Como iglesia tenemos la misión de servir y acompañar a la gente. Es por eso que ante la necesidad económica de algunas familias nació la iniciativa de hacer algo concreto. La asistente social nos animó a iniciar un trabajo comunitario al cual se le llamó grupo de diaconía.',
+    textMiddle: ' Con el transcurso de los años se lo conoce como "el rope-rito". En este espacio se realiza la entrega de ropa, calzado, útiles escolares, leche en polvo y otros elementos de uso cotidia no. Podemos hacer esta tarea gracias a los aportes y donaciones de la comunidad.',
+    textBottom: 'Damos gracias a Dios por el amor hacia el prójimo el cual es el mensaje de nuestro Señor Jesucristo.'
   },
   // CONTRATAPA
   {
